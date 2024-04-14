8e1b83a1b67a30741c0056896121f10b71e23a60
JanZmrzly <zmrzlyjan@gmail.com>
2024-04-14 23:30:49 +0200

    fix: api and logging

---
+++ main/api.py
@@ -0,0 +1,48 @@
+from vision import EdusetVision
+from eduset.utils.logger import EdusetLogger
+
+from fastapi import FastAPI, responses
+
+import uvicorn
+import tempfile
+import cv2 as cv
+
+app = FastAPI()
+logger = EdusetLogger("API")
+vision = EdusetVision()
+
+
+@app.get("/placement")
+async def read_placement(image: bool = False):
+    vision.update_placement()
+    placement = vision.placement
+    vision.disp_placement() if vision.verbose else None
+
+    if image:
+        with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as tmp:
+            img = vision.predicted_image
+            cv.imwrite(tmp.name, img)
+            logger.info("Predicted image sent")
+            return responses.FileResponse(tmp.name, media_type="image/png")
+
+    return responses.JSONResponse(content=placement)
+
+
+@app.get("/disconnect")
+async def disconnect():
+    vision.shut_down()
+    logger.warning("Camera disconnected")
+
+    return "Disconnected"
+
+
+@app.get("/connect")
+async def connect(ip: str = "emulation"):
+    vision.connect(ip)
+
+    return "Connected"
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
--- main/client.py
+++ main/client.py
@@ -1,8 +1,28 @@
 import asyncio
-from eduset.utils.opcua import main
+
+from eduset.utils.logger import EdusetLogger
+from eduset.utils.opcua import OPCUAClient
+
+
+logger = EdusetLogger("client")
+
+
+async def main():
+    url = "opc.tcp://192.168.0.10:4840"
+    api_endpoint = "http://localhost:8000/placement"
+
+    control_signal = {"ns": "http://EdusetONE",
+                      "i": 75}
+    placement = {"ns": "http://EdusetONE",
+                 "i": 19}
+
+    client = OPCUAClient(url=url)
+
+    try:
+        await client.set_encryption() if client.encryption is True else None
+        await client.run(control_signal, placement, time=2, api_endpoint=api_endpoint)
+    except KeyboardInterrupt:
+        await client.disconnect()
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.set_debug(True)
-    loop.run_until_complete(main())
-    loop.close()
+    asyncio.run(main())
